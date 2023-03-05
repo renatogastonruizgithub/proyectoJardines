0069995d30baab5407778288b3a81923145a0a86
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-03-04 19:45:58 -1100

    feat,login styled

--- jardin/next.config.js
+++ jardin/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+     images: {
+    domains: ['localhost']
+  },
 }
 
 module.exports = nextConfig
--- jardin/pages/index.js
+++ jardin/pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main >
       
       </main>
 
