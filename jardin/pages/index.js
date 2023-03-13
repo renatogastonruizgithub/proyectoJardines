461b68aa472375f1a623f91f45e76f53ff6020b1
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-03-12 20:17:03 -1100

    feat,styles contact

--- jardin/pages/index.js
+++ jardin/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Layout } from '../layouts/homePage/layout'
 import Banner from '../sections/banner'
+import { Contact } from '../sections/contact'
 import { Our } from '../sections/our'
 
 export default function Home() {
@@ -24,6 +25,7 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
           </p>
         <Our></Our>
+        <Contact></Contact>
       </Layout>   
     </>
   )
---
+++ jardin/sections/contact.jsx
@@ -0,0 +1,44 @@
+import { IconButton, Button, Container, Grid, Typography } from '@mui/material'
+import React from 'react'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+import Image from 'next/image';
+import { Box } from '@mui/system';
+
+
+
+
+export const Contact = () => {
+    return (
+        <section >
+            <Container maxWidth="lg">
+                <Grid container spacing={10}  >
+                    <Grid item sm={8} md={8} lg={8} xs={12} sx={{ display: "flex", alignItems: "center" }} >
+                        <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column", justifyContent: "center" }}>
+                            <Typography variant='h4' sx={{ fontWeight: "900", fontSize: { sm: "1.7rem", lg: "2rem" } }}>
+                                ¿Necesitas mas informacion?
+                            </Typography>
+                            <Typography variant='p' sx={{ fontSize: { sm: "1.1rem", lg: "1.2rem" }, marginTop: ".5rem" }}>
+                                !Estamos para ayudarte, ponte en contacto con nosotros!
+                            </Typography>
+
+                            <Button sx={{ marginTop: "2.2rem" }} variant="contained" size='large' >
+                                Escribinos por aqui
+                                <IconButton aria-label="delete" size="small">
+                                    <WhatsAppIcon sx={{ color: "#fff" }} fontSize="medium" />
+                                </IconButton>
+                            </Button>
+                        </Box>
+                    </Grid>
+                    <Grid item sm={4} md={4} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+                        <Image src="/assets/contact.png" width={300} height={300} />
+                    </Grid>
+                </Grid>
+
+            </Container>
+
+
+
+        </section>
+    )
+}
