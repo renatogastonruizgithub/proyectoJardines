1f4c6220d090163e169ad1b19d7b85046071ab1d
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-03-18 23:29:31 -1100

    feat, estructura de pagina noticias

---
+++ jardin/pages/noticias.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { LayuotSecondary } from '../layouts/secondaryPages/layuotSecondary'
+import { BannerLayouts } from '../sections/bannerLayouts'
+
+const Noticias = () => {
+    return (
+        <>
+            <LayuotSecondary>
+                <BannerLayouts></BannerLayouts>
+                <div>
+                    <h1>Noticias</h1>
+                </div>
+            </LayuotSecondary>
+
+        </>
+    )
+}
+export default Noticias
