8af2e95a5aee3488529249a68ce4069e88f2709d
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-03-18 23:11:33 -1100

    feat,componente banner dinamico para paginas

---
+++ jardin/sections/bannerLayouts.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import React from 'react'
+import banner from "../styles/bannerLayouts.module.scss"
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+
+export const BannerLayouts = () => {
+    const itemData = [
+        {
+            img: "/assets/secondariBanner.jpg",
+
+        }
+    ]
+    return (
+        <>
+            <section className={banner.contenetBanner}>
+                <div role="presentation" >
+                    <Breadcrumbs separator="›" color="#fff" sx={{ fontSize: "1.2rem", letterSpacing: "1px" }} aria-label="breadcrumb" >
+                        <Link underline="hover" color="#fff" href="/">
+                            Home
+                        </Link>
+                        <Link
+                            underline="hover"
+                            color="#fff"
+                            href="/material-ui/getting-started/installation/"
+                        >
+                            Galeria
+                        </Link>
+                    </Breadcrumbs>
+                </div>
+                {
+                    itemData.map((item) =>
+                        <Image key={item} src={item.img} size fill="100vw" />
+                    )
+                }
+            </section>
+        </>
+    )
+}
