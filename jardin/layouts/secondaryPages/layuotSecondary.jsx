3e08b5ffc9fd39d52f35ddf6fb09749e859f1641
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-03-25 22:04:11 -1100

    fix,refactor provider component home

--- jardin/layouts/secondaryPages/layuotSecondary.jsx
+++ jardin/layouts/secondaryPages/layuotSecondary.jsx
@@ -3,21 +3,23 @@ import React from 'react'
 import { Contact } from '../../sections/contact'
 import Footer from '../../sections/footer'
 import Navbar from '../../sections/navbar'
-
+import { ProviderComponent } from "../../context/contextHome";
 
 export const LayuotSecondary = ({ children }) => {
     return (
         <>
-            <nav>
-                <Navbar />
-            </nav>
-            <main>
-                {children}
-            </main>
-            <footer>
-                <Contact></Contact>
-                <Footer />
-            </footer>
+            <ProviderComponent>
+                <nav>
+                    <Navbar />
+                </nav>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    <Contact></Contact>
+                    <Footer />
+                </footer>
+            </ProviderComponent>
         </>
     )
 }
