a75da12762e4d0c4644b51db2332fb9b01098ccb
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-04-09 12:00:49 -1100

    fix,refactor stilos responsive y sidebar mobile

--- jardin/layouts/adminPages/componentesAdmin/Sidebar.jsx
+++ jardin/layouts/adminPages/componentesAdmin/Sidebar.jsx
@@ -16,100 +16,109 @@ import PublishIcon from '@mui/icons-material/Publish';
 import FolderIcon from '@mui/icons-material/Folder';
 import HomeIcon from '@mui/icons-material/Home';
 import { useRouter } from 'next/navigation';
-
+import { useState } from "react";
+import { useMobile } from "../../../context/contextMenuMobile"
 
 const Sidebar = () => {
-    const [open, setOpen] = React.useState(true);
+    const { mobileOpen, setMobileOpen } = useMobile();
+    const [openItems, setOpenItems] = useState(false);
     const router = useRouter();
 
-    const handleClick = () => {
-        setOpen(!open);
+    const handleExpand = () => {
+        setOpenItems(!openItems);
     };
 
     const Home = () => {
         router.push("/admin/dashboard")
+        setMobileOpen(!mobileOpen)
     };
     const Empresa = () => {
         router.push("/admin/empresa/empresa")
+        setMobileOpen(!mobileOpen)
     };
     const Empleado = () => {
         router.push("/admin/empleados/empleado")
+        setMobileOpen(!mobileOpen)
     };
     const Publicacion = () => {
         router.push("/admin/publicaciones/publicacion")
+        setMobileOpen(!mobileOpen)
     };
     const Proyecto = () => {
         router.push("/admin/proyectos/proyecto")
+        setMobileOpen(!mobileOpen)
     };
     const Galeria = () => {
         router.push("/admin/gallery/addPhotos")
+        setMobileOpen(!mobileOpen)
     };
     const viewPhotos = () => {
         router.push("/admin/gallery/viewPhotos")
+        setMobileOpen(!mobileOpen)
     };
     return (
         <>
-            <section className='sideBar'>
-                <List
-                    sx={{ top: "80px", color: "#fff", width: '100%', height: "100vh", maxWidth: 360, bgcolor: 'rgb(74, 125, 196)' }}
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                >
-                    <ListItemButton onClick={Home}>
-                        <ListItemIcon>
-                            <HomeIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Home" />
-                    </ListItemButton>
-                    <ListItemButton onClick={Empresa}>
-                        <ListItemIcon>
-                            <BusinessIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Empresa" />
-                    </ListItemButton>
-                    <ListItemButton onClick={Empleado}>
-                        <ListItemIcon>
-                            <PeopleAltIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Empleado" />
-                    </ListItemButton>
-                    <ListItemButton onClick={Publicacion}>
-                        <ListItemIcon>
-                            <PublishIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Publicacion" />
-                    </ListItemButton>
-                    <ListItemButton onClick={Proyecto}>
-                        <ListItemIcon>
-                            <FolderIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Proyecto" />
-                    </ListItemButton>
-                    <ListItemButton onClick={handleClick}>
-                        <ListItemIcon>
-                            <PhotoLibraryIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Galeria" />
-                        {open ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <ListItemButton onClick={Galeria} sx={{ pl: 4 }}>
-                                <ListItemIcon>
-                                    <AddAPhotoIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="agregar fotos" />
-                            </ListItemButton>
-                            <ListItemButton onClick={viewPhotos} sx={{ pl: 4 }}>
-                                <ListItemIcon>
-                                    <PhotoIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="ver fotos" />
-                            </ListItemButton>
-                        </List>
-                    </Collapse>
-                </List>
-            </section>
+
+            <List
+
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+            >
+                <ListItemButton onClick={Home}>
+                    <ListItemIcon>
+                        <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Home" />
+                </ListItemButton>
+                <ListItemButton onClick={Empresa}>
+                    <ListItemIcon>
+                        <BusinessIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Empresa" />
+                </ListItemButton>
+                <ListItemButton onClick={Empleado}>
+                    <ListItemIcon>
+                        <PeopleAltIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Empleado" />
+                </ListItemButton>
+                <ListItemButton onClick={Publicacion}>
+                    <ListItemIcon>
+                        <PublishIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Publicacion" />
+                </ListItemButton>
+                <ListItemButton onClick={Proyecto}>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Proyecto" />
+                </ListItemButton>
+                <ListItemButton onClick={handleExpand}>
+                    <ListItemIcon>
+                        <PhotoLibraryIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Galeria" />
+                    {openItems ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                <Collapse in={openItems} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemButton onClick={Galeria} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <AddAPhotoIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="agregar fotos" />
+                        </ListItemButton>
+                        <ListItemButton onClick={viewPhotos} sx={{ pl: 4 }}>
+                            <ListItemIcon>
+                                <PhotoIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="ver fotos" />
+                        </ListItemButton>
+                    </List>
+                </Collapse>
+            </List>
+
 
         </>
     )
